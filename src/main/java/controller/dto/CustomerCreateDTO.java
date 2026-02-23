aeb0b626359c5d8179a200f8b5a37501247ade55
Luyen <luyenlaluot@gmail.com>
2026-02-23 12:16:17 +0700

    add dto create customer file

---
+++ src/main/java/controller/dto/CustomerCreateDTO.java
@@ -0,0 +1,177 @@
+package controller.dto;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class CustomerCreateDTO {
+
+    /*
+     * =========================
+     * BASIC INFO
+     * =========================
+     */
+    private String name;
+    private String phone;
+    private String gender;
+    private String email;
+    private LocalDate birthday;
+    private String socialLink;
+    private String address;
+
+    /*
+     * =========================
+     * FIT PROFILE
+     * =========================
+     */
+    private Integer height;
+    private Integer weight;
+    private String preferredSize;
+
+    private Integer bust;
+    private Integer waist;
+    private Integer hips;
+    private Integer shoulder;
+
+    private String bodyShape;
+
+    /*
+     * =========================
+     * STYLE TAGS
+     * =========================
+     */
+    private List<String> styleTags;
+
+    /*
+     * =========================
+     * GETTER & SETTER
+     * =========================
+     */
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getSocialLink() {
+        return socialLink;
+    }
+
+    public void setSocialLink(String socialLink) {
+        this.socialLink = socialLink;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Integer getHeight() {
+        return height;
+    }
+
+    public void setHeight(Integer height) {
+        this.height = height;
+    }
+
+    public Integer getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Integer weight) {
+        this.weight = weight;
+    }
+
+    public String getPreferredSize() {
+        return preferredSize;
+    }
+
+    public void setPreferredSize(String preferredSize) {
+        this.preferredSize = preferredSize;
+    }
+
+    public Integer getBust() {
+        return bust;
+    }
+
+    public void setBust(Integer bust) {
+        this.bust = bust;
+    }
+
+    public Integer getWaist() {
+        return waist;
+    }
+
+    public void setWaist(Integer waist) {
+        this.waist = waist;
+    }
+
+    public Integer getHips() {
+        return hips;
+    }
+
+    public void setHips(Integer hips) {
+        this.hips = hips;
+    }
+
+    public Integer getShoulder() {
+        return shoulder;
+    }
+
+    public void setShoulder(Integer shoulder) {
+        this.shoulder = shoulder;
+    }
+
+    public String getBodyShape() {
+        return bodyShape;
+    }
+
+    public void setBodyShape(String bodyShape) {
+        this.bodyShape = bodyShape;
+    }
+
+    public List<String> getStyleTags() {
+        return styleTags;
+    }
+
+    public void setStyleTags(List<String> styleTags) {
+        this.styleTags = styleTags;
+    }
+}
